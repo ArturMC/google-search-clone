141826597c852809fa4bd38c22f056ca611cda6d
ArturMC <amchojecki@gmail.com>
2021-11-08 17:27:20 -0500

    Finish header and add avatar and hero icons

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,9 +1,11 @@
-function Avatar() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Avatar({url}) {
+    return <img 
+        loading="lazy"
+        src={url}
+        alt="profile picture"
+        className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110"
+        />
+    
 }
 
 export default Avatar
